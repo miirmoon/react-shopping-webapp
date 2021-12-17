e341ad869e27fd6c7e803469679bb5e3d139301e
신미래 <sml6209@naver.com>
2021-12-17 17:41:12 +0900

    fix: project name을 basename으로 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
-import { Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import ShoppingBasket from "./pages/ShoppingBasket";
 
 function App() {
   return (
-    <div>
-      <Link to="/">홈</Link>
-      <Link to="/product">상세페이지</Link>
-      <Link to="/basket">장바구니</Link>
+    <BrowserRouter basename="/react-shopping-webapp">
+      <div>
+        <Link to="/">홈</Link>
+        <Link to="/product">상세페이지</Link>
+        <Link to="/basket">장바구니</Link>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<ProductDetail />} />
-        <Route path="/basket" element={<ShoppingBasket />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<ProductDetail />} />
+          <Route path="/basket" element={<ShoppingBasket />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
