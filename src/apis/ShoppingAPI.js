dc0d99d3105f9029fd62487d79dfb06571d4c4a0
신미래 <sml6209@naver.com>
2022-01-03 00:23:47 +0900

    [chore]: 환경변수 관리 파일 추가

--- src/apis/ShoppingAPI.js
+++ src/apis/ShoppingAPI.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dc243b7a-2384-42f1-9fcd-a8399e97303b.mock.pstmn.io/",
+  baseURL: process.env.REACT_APP_PSTM_API,
 });
 
+// 테마 목록 불러오기
 export function getThemes(success, fail) {
   api.get("themes/popular").then(success).catch(fail);
 }
 
+// 테마Id에 해당하는 상품 목록 불러오기
 export function getThemeProducts(themeId, success, fail) {
   api.get(`themes/${themeId}/products`).then(success).catch(fail);
 }
